462a18aaf47d504b70a43c5ed91d5d516cafa08b
Hanul Kim <s2122@e-mirim.hs.kr>
2023-04-17 11:57:59 +0900

    setcursorposition  연습

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("메서드 호출1");
+            Console.SetCursorPosition(5, 5);
+            Console.WriteLine("메서드 호출2");
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("메서드 호출3");
+        }
+    }
+}
